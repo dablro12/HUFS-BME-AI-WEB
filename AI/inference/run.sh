b112ec75ab28e3c39a7cfda38078f227c5e0a2b8
dablro12 <dablro1232@gmail.com>
2024-06-07 16:39:21 +0900

    setup model

--- AI/inference/run.sh
+++ AI/inference/run.sh
@@ -8,7 +8,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 python3 "$DIR/script.py" \
     --env "APPLE" \
-    --project_dir "/Users/jeong-yeongjin/HUFS-BME-AI-WEB/HUFS-BME-AI-WEB" \
+    --project_dir "../../" \
     --img_dir 'WEB/inference/img' \
     --mask_dir 'WEB/inference/mask' \
     --model_name "$1" \
