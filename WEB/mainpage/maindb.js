dc32330412f2b942fb3135995622c14146f6e9a8
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-08-07 11:15:23 +0900

    Update maindb.js

--- WEB/mainpage/maindb.js
+++ WEB/mainpage/maindb.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var maindb = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'dudwls1234',
+    password: '',
     database: 'crud'
 });
 maindb.connect();
